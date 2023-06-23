5d4b8c5dd080b584560a11a0b5fde0203229e5c8
Blaine Miller <blaine@blaineam.com>
2023-06-22 22:00:40 -0700

    optimize multi threading to not let cores idle

--- changepassphrase.php
+++ changepassphrase.php
@@ -67,88 +67,52 @@ function get_processor_cores_number() {
 
 $usableCores = round(get_processor_cores_number() * 0.75);
 
+$tasks = array_slice($files, $progress);
 
-function runBatch($files, $newpassphrase, $passphrase, $cores) {
-    $procs = [];
-    $pipes = [];
+$procs = [];
+$pipes = [];
 
-    $cmd = "php ./changepassphrase-worker.php -p $passphrase -n $newpassphrase -f";
+$cmd = "php ./changepassphrase-worker.php -p $passphrase -n $newpassphrase -f";
 
-    $desc = [
-        0 => [ 'pipe', 'r' ],
-        1 => [ 'pipe', 'w' ],
-        2 => [ 'pipe', 'a' ],
-    ];
-
-    if (count($files) > $cores) {
-        throw Exception("batch is too big");
+$desc = [
+    0 => [ 'pipe', 'r' ],
+    1 => [ 'pipe', 'w' ],
+    2 => [ 'pipe', 'a' ],
+];
+while ( $procs || count($tasks) > 0 ) {
+    if(count($procs) < $usableCores) {
+        $usedKeys = array_keys($procs);
+        $possibleKeys =  range(0, $usableCores);
+        $freeKeys = array_diff($possibleKeys, $usedKeys);
+        foreach($freeKeys as $i) {
+            $taskFile = array_shift($tasks);
+            $iCmd = $cmd . ' "' . $taskFile . '"';
+            $proc = proc_open($iCmd, $desc, $pipes[$i], __DIR__);
+            $procs[$i] = $proc;
+        }   
     }
-
-    for ( $i = 0; $i < count($files); $i++ ) {
-        $iCmd = $cmd . ' "' . $files[$i] . '"';
-        $proc = proc_open($iCmd, $desc, $pipes[ $i ], __DIR__);
-
-        $procs[ $i ] = $proc;
-    }
-
     $stdins = array_column($pipes, 0);
     $stdouts = array_column($pipes, 1);
     $stderrs = array_column($pipes, 2);
-
-    while ( $procs ) {
-        foreach ( $procs as $i => $proc ) {
-            // @gzhegow > [OR] you can output while script is running (if child never finishes)
-            $read = [ $stdins[ $i ] ];
-            $write = [ $stdouts[ $i ], $stderrs[ $i ] ];
-            $except = [];
-            if (stream_select($read, $write, $except, $seconds = 0, $microseconds = 1000)) {
-                foreach ( $write as $stream ) {
-                    echo stream_get_contents($stream);
-                }
+    foreach ( $procs as $i => $proc ) {
+        $status = proc_get_status($proc);
+        if (false === $status[ 'running' ]) {
+            if ($content = stream_get_contents($stderrs[ $i ])) {
+                echo '[ERROR]' . $content . PHP_EOL;
             }
-
-            $status = proc_get_status($proc);
-
-            if (false === $status[ 'running' ]) {
-                $status = proc_close($proc);
-                unset($procs[ $i ]);
-
-                echo 'STATUS: ' . $status . PHP_EOL;
+            echo stream_get_contents($stdouts[ $i ]) . PHP_EOL;
+            $status = proc_close($proc);
+            unset($procs[ $i ]);
+            $progress++;
+            file_put_contents($cachePath, json_encode(["progress" => $progress, "files" => $files]));
+            $percent = round(($progress/$filesCount) * 100, 2);
+            if($percent - $lastPercent >= 2) {
+                echo PHP_EOL.PHP_EOL. "Overall Progress: {$percent}%".PHP_EOL.PHP_EOL;
+                $lastPercent = $percent;
             }
-
-            // @gzhegow > [OR] you can output once command finishes
-            // $status = proc_get_status($proc);
-            //
-            // if (false === $status[ 'running' ]) {
-            //     if ($content = stream_get_contents($stderrs[ $i ])) {
-            //         echo '[ERROR]' . $content . PHP_EOL;
-            //     }
-            //
-            //     echo stream_get_contents($stdouts[ $i ]) . PHP_EOL;
-            //
-            //     $status = proc_close($proc);
-            //     unset($procs[ $i ]);
-            //
-            //     echo 'STATUS: ' . $status . PHP_EOL;
-            // }
         }
-
-        usleep(1); // give your computer one tick to decide what thread should be used
     }
-
-}
-
-foreach(array_chunk(array_slice($files, $progress), $usableCores) as $index => $filesChunk) {
-    echo PHP_EOL."starting batch: $index";
-    runBatch($filesChunk, $newpassphrase, $passphrase, $usableCores);
-    echo PHP_EOL."Finished batch: $index";
-    $progress += $usableCores;
-    file_put_contents($cachePath, json_encode(["progress" => $progress, "files" => $files]));
-    $percent = round(($progress/$filesCount) * 100, 2);
-    if($percent - $lastPercent >= 2) {
-        echo PHP_EOL.PHP_EOL. "Overall Progress: {$percent}%".PHP_EOL.PHP_EOL;
-        $lastPercent = $percent;
-    }
+    usleep(1);
 }
 
 unlink($cachePath);
